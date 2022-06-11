fb32560faa34b2a6773255468566e2a598b68999
lilhalzy <lilhalzy11@gmail.com>
2022-06-11 12:14:57 +0800

    Animated the texts and logo

--- animation/Text Animation 2/app.js
+++ animation/Text Animation 2/app.js
@@ -13,3 +13,15 @@ timeline.fromTo('.cta4', {x: '100%', opacity: .5}, {x: 0, opacity:1}, '<25%');
 timeline.fromTo('.cta6', {x: '-100%', opacity: .5}, {x: 0, opacity:1}, '<25%');
 timeline.fromTo('.cta5', {y: '100%', opacity: .5}, {y: 0, opacity:1}, '<25%');
 timeline.fromTo('.cta-btn', {y: 20, opacity: 0} , {y: 0, opacity: 1})
+
+const logo = document.querySelector('.logo');
+const letters = logo.textContent.split('');
+
+logo.textContent = ""
+
+letters.forEach(letter => {
+  logo.innerHTML += `<span class='letter'>` + letter + `</span>`;
+})
+
+gsap.set('.letter', {display: 'inline-block'});
+gsap.fromTo('.letter', {y: 30}, {y: 0, delay: 3, stagger: .05})
