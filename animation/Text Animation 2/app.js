87768fccd8ac475b839c9ec96e85cdc175acd9b7
lilhalzy <lilhalzy11@gmail.com>
2022-06-11 11:37:54 +0800

    Added animations for each text and button

--- animation/Text Animation 2/app.js
+++ animation/Text Animation 2/app.js
@@ -4,4 +4,12 @@ timeline.fromTo('.hero-img', {scale: 1.5, borderRadius: '0rem'}, {
   scale: 1, borderRadius: '5.4rem',
   duration: 3,
   ease: 'elastic.out(1.6, 1)'
-})
+});
+
+timeline.fromTo('.cta1', {x: '100%', opacity: .5}, {x: 0, opacity:1}, '<25%');
+timeline.fromTo('.cta3', {x: '-100%', opacity: .5}, {x: 0, opacity:1}, '<25%');
+timeline.fromTo('.cta2', {y: '100%', opacity: .5}, {y: 0, opacity:1}, '<25%');
+timeline.fromTo('.cta4', {x: '100%', opacity: .5}, {x: 0, opacity:1}, '<25%');
+timeline.fromTo('.cta6', {x: '-100%', opacity: .5}, {x: 0, opacity:1}, '<25%');
+timeline.fromTo('.cta5', {y: '100%', opacity: .5}, {y: 0, opacity:1}, '<25%');
+timeline.fromTo('.cta-btn', {y: 20, opacity: 0} , {y: 0, opacity: 1})
