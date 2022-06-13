a100c5fb00ddcafbb3e7d6403b17d2bd1652f928
lilhalzy <lilhalzy11@gmail.com>
2022-06-13 11:19:01 +0800

    Animated the Home title individually

--- animation/Micro Animation/app.js
+++ animation/Micro Animation/app.js
@@ -1,6 +1,8 @@
 const timeline = gsap.timeline({defaults: {duration: .4, ease: 'Power.easeOut'}})
 
 const home = document.querySelector('.home')
+const homeTitle = document.querySelector('.home-title')
+const homeLetters = homeTitle.textContent.split('')
 
 gsap.set('.feather', {scale: 0, transformOrigin: 'center'})
 home.addEventListener('click', () => {
@@ -9,4 +11,16 @@ home.addEventListener('click', () => {
   gsap.fromTo('.right-feather', {x: 0}, {x: 5})
   gsap.fromTo('.left-feather', {x: 0}, {x: -5})
   gsap.fromTo('.roof', {y: 0}, {y: 6, yoyo: true, repeat: 1})
+  gsap.set(".letter", { display: "inline-block" });
+  gsap.fromTo(
+    ".letter",
+    { y: 0 },
+    { y: "-40%", stagger: 0.04, yoyo: true, repeat: 1 }, '<'
+  );
+})
+
+
+homeTitle.textContent = ""
+homeLetters.forEach(letter => {
+  homeTitle.innerHTML += `<span class='letter'>` + letter + `</span>`
 })
