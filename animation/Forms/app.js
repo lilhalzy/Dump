eddb5180023a8af4df2292d4dc0b154b9ca6d3af
lilhalzy <lilhalzy11@gmail.com>
2022-06-24 08:40:18 +0800

    Created the line elastic animations once the input be focused on

--- animation/Forms/app.js
+++ animation/Forms/app.js
@@ -8,3 +8,18 @@ const start =
   "M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512";
 const end =
   "M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512";
+
+// Elastic effect
+containers.forEach(container => {
+  const input = container.querySelector('.input')
+  const line = container.querySelector('.elastic-line')
+  const placeholder = container.querySelector('.placeholder')
+
+    input.addEventListener('focus', () => {
+      // Check to see if there is any text in the input
+      if(!input.value) {
+        timeline.fromTo(line, {attr: {d: start}}, {attr: {d:end}, ease: "Power2.easeOut", duration:.75})
+        timeline.to(line, {attr: {d:start}, ease:"elastic.out(2.5, .5)"}, '<50%')
+      }
+    })
+})
