c52c3ad9b839542b55607053ceaae348db7340b9
lilhalzy <lilhalzy11@gmail.com>
2022-06-25 21:08:54 +0800

    Added email validations

--- animation/Forms/app.js
+++ animation/Forms/app.js
@@ -64,3 +64,14 @@ form.addEventListener("click", () => {
     }
   });
 });
+
+// Email validation
+function validateEmail(email) {
+  let re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+function validatePhone(phone) {
+  let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return re.test(phone);
+}
