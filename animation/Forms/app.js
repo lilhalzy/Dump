25b1ef4b878b554f1a641c043dca2da369ade93e
lilhalzy <lilhalzy11@gmail.com>
2022-06-27 17:34:28 +0800

    Make the promotion text colored after the checkbox ticked

--- animation/Forms/app.js
+++ animation/Forms/app.js
@@ -136,8 +136,10 @@ checkbox.addEventListener("click", () => {
       { strokeDashoffset: 0 }, 
       '<50%'
     );
+    timeline.to('.checkbox-label', {color: '#6391e8'}, '<')
   } else {
     timeline2.to('.checkbox-fill', {top: '100%'})
     timeline.fromTo(tickMarkPath, {strokeDashoffset: 0}, {strokeDashoffset: pathLength}, '<50%')
+      timeline.to(".checkbox-label", { color: "#c5c5c5" }, "<");
   }
 });
